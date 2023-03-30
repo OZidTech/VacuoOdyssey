0c724bae3bf323bcee61cde3e728482d0136089b
JO0O03 <Bojofo2009@gmail.com>
2023-03-29 20:01:13 -0500

    working vacuum
    working vacuum

---
+++ VacouOdyssey/Assets/Bobby'sWorkSpace/Scripts/AbilityGet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityGet : MonoBehaviour
+{
+    public GameObject vacuum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vacuum.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Mouse1))
+        {
+            vacuum.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            vacuum.SetActive(false);
+        }
+
+    }
+}
